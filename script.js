b173bbc86a8d84cd09eddf91eb38a77fd31fdf40
Guerschom-Tambwe <guerschomguerschom@gmail.com>
2024-01-07 14:49:21 +0200

    restyled quote generator and added the loading functionality

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true
+}
+
 function newQuote() {
+    loading();
     const quote  = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     if(!quote.author) {
         authorText.textContent = 'Unknown';
@@ -21,10 +33,12 @@ function newQuote() {
     }
     
     quoteText.textContent = quote.text;
+    complete();
 
 }
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
       const response = await fetch(apiUrl);
